defeac367125e3671eb20c13689a608d4f822394
kevinzhaosmart <SachezShepardson926@hotmail.com>
2025-12-25 14:26:18 +0800

    Update deploy script for OblivionStore

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedStore = await deploy("OblivionStore", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`OblivionStore contract: `, deployedStore.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_oblivionStore"; // id required to prevent reexecution
+func.tags = ["OblivionStore"];
